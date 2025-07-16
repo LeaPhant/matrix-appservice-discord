03503f44affaa62b2f2a5c1fcfaf37672aab0236
ElwinghL <clement_1996@hotmail.com>
2025-07-16 21:42:59 +0200

    Add the case where there is a role in formatted message

--- src/parser/matrixmessageparser.ts
+++ src/parser/matrixmessageparser.ts
@@ -33,7 +33,7 @@ const log = new Log("DiscordMessageParser");
 export interface IMatrixMessageParserCallbacks {
     canNotifyRoom: () => Promise<boolean>;
     getUserId: (mxid: string) => Promise<string | null>;
-    getRoleId:(mxid: string) => Promise<string | null>;
+    getRoleId: (mxid: string) => Promise<string | null>;
     getChannelId: (mxid: string) => Promise<string | null>;
     getEmoji: (mxc: string, name: string) => Promise<IDiscordEmoji | null>;
     mxcUrlToHttp: (mxc: string) => Promise<string | null>;
@@ -78,23 +78,12 @@ export class MatrixMessageParser {
             const parsed = Parser.parse(msg.formatted_body, {
                 lowerCaseTagName: true,
                 pre: true,
-            // tslint:disable-next-line no-any
+                // tslint:disable-next-line no-any
             } as any);
             reply = await this.walkNode(opts, parsed);
             reply = reply.replace(/\s*$/, ""); // trim off whitespace at end
         } else {
-            reply = await this.escapeDiscord(opts, msg.body);
-
-            //Look for roles and matches them
-            const potentRole = new RegExp(/@(?<potentRole>[A-Za-z0-9]+)/g);
-            for (const match of reply.matchAll(potentRole)) {
-                if(match.groups && match.groups.potentRole) {
-                    const result = await this.parseRole(opts,match.groups.potentRole);
-                    if (result) {
-                        reply = reply.replace(match[0],result);
-                    }
-                }
-            }
+            reply = await this.lookForRolesAndEscapeDiscord(opts, msg.body);
         }
 
         if (msg.msgtype === "m.emote") {
@@ -103,6 +92,28 @@ export class MatrixMessageParser {
                 reply = `_${await this.escapeDiscord(opts, opts.displayname)} ${reply}_`;
             } else {
                 reply = `_${reply}_`;
+            }
+        }
+        return reply;
+    }
+
+    private async lookForRolesAndEscapeDiscord(opts: IMatrixMessageParserOpts, msg: string) {
+        let reply = await this.escapeDiscord(opts, msg);
+        reply = await this.lookForAndMatchRoles(opts, reply);
+
+        return reply;
+    }
+
+    private async lookForAndMatchRoles(opts: IMatrixMessageParserOpts, msg: string) {
+        let reply = ""
+        //Look for roles and matches them
+        const potentRole = new RegExp(/@(?<potentRole>[A-Za-z0-9]+)/g);
+        for (const match of msg.matchAll(potentRole)) {
+            if (match.groups && match.groups.potentRole) {
+                const result = await this.parseRole(opts, match.groups.potentRole);
+                if (result) {
+                    reply = msg.replace(match[0], result);
+                }
             }
         }
         return reply;
@@ -372,9 +383,8 @@ export class MatrixMessageParser {
             if ((node as Parser.TextNode).text === "\n") {
                 return "";
             }
-            //If it's not a link and it start with @ it can be
 
-            return await this.escapeDiscord(opts, (node as Parser.TextNode).text);
+            return await this.lookForRolesAndEscapeDiscord(opts, (node as Parser.TextNode).text);
         } else if (node.nodeType === Parser.NodeType.ELEMENT_NODE) {
             const nodeHtml = node as Parser.HTMLElement;
             switch (nodeHtml.tagName) {
